8627c3bf528e001e4684d8f00a9a6728d9b1223f
Max Reshetey <maxim.reshetey@gmail.com>
2017-12-20 15:51:38 +0300

    Initial Commit

---
+++ WaterfallTests/WaterfallTests.swift
@@ -0,0 +1,36 @@
+//
+//  WaterfallTests.swift
+//  WaterfallTests
+//
+//  Created by Max Reshetey on 20/12/2017.
+//  Copyright © 2017 Max Reshetey. All rights reserved.
+//
+
+import XCTest
+@testable import Waterfall
+
+class WaterfallTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
